e1725377499ef1a4a81b0f3c730ec16bb3e5d0fe
Suman Guha <sumanguha1989@gmail.com>
2023-09-24 19:01:03 +0530

    Add DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb+srv://sgmagic:My_Mongo_89@cluster0.gtjqwjc.mongodb.net/?retryWrites=true&w=majority')],
   controllers: [AppController],
   providers: [AppService],
 })
