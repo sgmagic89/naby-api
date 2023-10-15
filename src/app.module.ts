4206da98920ff7f1bdc1fbb3b676a9842e260adf
Suman Guha <sumanguha1989@gmail.com>
2023-10-15 13:13:09 +0530

    Mongo Connection through env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,40 @@
+import { UsersModule } from './users/users.module';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { config } from './config/config';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb+srv://sgmagic:My_Mongo_89@cluster0.gtjqwjc.mongodb.net/?retryWrites=true&w=majority',
-  {dbName: 'nabyindb'})],
+  imports: [
+    UsersModule, 
+    MongooseModule.forRoot('mongodb+srv://sgmagic:My_Mongo_89@cluster0.gtjqwjc.mongodb.net/?retryWrites=true&w=majority',
+      {  }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        uri: config.get<string>('mongodb.database.connectionString'),
+        dbName: config.get<string>('mongodb.database.databaseName')
+      })
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [config]
+    }),
+    HttpModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        baseURL: configService.get<string>('api.apiUrl'),
+        timeout: configService.get<number>('api.httpTimeout')
+      }),
+      inject: [ConfigService]
+    })
+    ],
+      
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = new DocumentBuilder()
+  .setTitle('NabyIn API')
+  .setDescription('APIs for NabyIn App')
+  .setVersion('1.0')
+  .addTag('users')
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('port');
+  await app.listen(port);
+  Logger.log(`~ Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
